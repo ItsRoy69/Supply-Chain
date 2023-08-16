9e9bba894d9692a66d5851171d7df73e9816ab6c
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-08-16 23:13:00 +0530

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Row } from "antd";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.css";
 import { useUserContext } from "../../context/userContext";
 import Logo from "../Logo";
 
